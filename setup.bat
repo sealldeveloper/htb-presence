a30b47f39b6e47d33438f1dbe2ee19b6f6ba94b9
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2024-05-31 16:13:38 +1000

    workshop new VBScript solution to silent startup windows

--- setup.bat
+++ setup.bat
@@ -1,3 +1,14 @@
 @echo off
 rem Add Python script to startup
-echo python "%~dp0htb-presence.py" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\htb-presence-startup.bat"
+echo pythonw "%~dp0htb-presence.py" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\htb-presence-startup.bat"
+
+@REM @echo off
+@REM rem Create a VBScript to launch Python script invisibly
+@REM echo Set objShell = CreateObject("WScript.Shell") > "%TEMP%\RunPythonScript.vbs"
+@REM echo objShell.Run "pythonw ""%~dp0htb-presence.py""", 0, False >> "%TEMP%\RunPythonScript.vbs"
+
+@REM rem Add VBScript to startup
+@REM move /y "%TEMP%\RunPythonScript.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+
+@REM rem Clean up temporary VBScript file
+@REM del "%TEMP%\RunPythonScript.vbs"
