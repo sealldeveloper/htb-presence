f52aaef47f60d47cd81e8407f37286456ef5b124
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2024-05-31 16:20:14 +1000

    revise and check for existing file, remove it beforehand.

--- setup.bat
+++ setup.bat
@@ -1,14 +1,16 @@
 @echo off
+
+rem Set the file path
+set "file_path=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\htb-presence-startup.bat"
+
+rem Check if the file exists
+if exist "%file_path%" (
+    rem File exists, so delete it
+    del "%file_path%"
+    echo Old startup script deleted.
+)
+
 rem Add Python script to startup
-echo pythonw "%~dp0htb-presence.py" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\htb-presence-startup.bat"
-
-@REM @echo off
-@REM rem Create a VBScript to launch Python script invisibly
-@REM echo Set objShell = CreateObject("WScript.Shell") > "%TEMP%\RunPythonScript.vbs"
-@REM echo objShell.Run "pythonw ""%~dp0htb-presence.py""", 0, False >> "%TEMP%\RunPythonScript.vbs"
-
-@REM rem Add VBScript to startup
-@REM move /y "%TEMP%\RunPythonScript.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
-
-@REM rem Clean up temporary VBScript file
-@REM del "%TEMP%\RunPythonScript.vbs"
+echo :loop >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\htb-presence-startup.bat"
+echo python "%~dp0htb-presence.py" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\htb-presence-startup.bat"
+echo goto loop >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\htb-presence-startup.bat"
